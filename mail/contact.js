b4afca10fa6876b3acd4fb0cb104527ac524ab32
Abdul <jkglobalitsolutionsweb1@gmail.com>
2024-07-15 12:16:51 +0530

    Changes Made

--- mail/contact.js
+++ mail/contact.js
@@ -35,12 +35,12 @@ $(function () {
                     $('#contactForm').trigger("reset");
                 },
                 error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
+                    // $('#success').html("<div class='alert alert-danger'>");
+                    // $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    //         .append("</button>");
+                    // // $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    // $('#success > .alert-danger').append('</div>');
+                    // $('#contactForm').trigger("reset");
                 },
                 complete: function () {
                     setTimeout(function () {
